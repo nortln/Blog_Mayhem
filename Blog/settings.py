d496c56e9da3b7c2a57652d0d1480d754aa1f005
nortln <clintontristanacje@gmail.com>
2023-09-29 23:51:45 +0100

    sixteenth

--- Blog/settings.py
+++ Blog/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 with open("secret_key.txt", "r") as f:
     SECRET_KEY = f.read().strip()
@@ -109,6 +110,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
